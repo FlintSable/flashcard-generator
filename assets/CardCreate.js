e970d4ba6874777024dd030d079101ebfa41eadb
Nicholas N <nnlaor@gmail.com>
2017-04-19 00:22:54 -0700

    updating

--- assets/CardCreate.js
+++ assets/CardCreate.js
@@ -10,13 +10,14 @@ var Card = function(guid, question, answer) {
 
 exports.createFlash = function(arg1, arg2) {
     flashy(arg1, arg2);
-
 };
+
 
 count = 0;
 
 // get this to log out to json file
 var obj = [];
+
 
 function flashy(x, arg2) {
     var file = './assets/decks/' + arg2 + '.deck';
@@ -27,41 +28,48 @@ function flashy(x, arg2) {
         }, {
             name: 'answer',
             message: "what would you like the back of the flash card to say?"
-        }, {
-            name: 'placeholder',
-            message: "placeholder:"
-        }, {
-            name: "placeholder2",
-            message: "placeholder2?"
         }]).then(function(answers) {
             guid = arg2;
             var flash = new Card(guid + count, answers.question, answers.answer);
             count++;
             flash = JSON.stringify(flash);
-            obj.push(flash + '\n');
+            obj.push(flash);
             flashy(x, guid);
         });
 
-        } 
-        else if (count === x) {
+    } else if (count === x) {
+        fs.appendFile(file, '['+ obj + ']', function(err) {
+            if (err) throw err;
+            console.error(err);
+        });
         inquirer.prompt([{
             name: 'study',
             message: 'would you like to study this deck',
             type: 'confirm'
+        }]).then(function(answers) {
+            console.log('\ntime to study');
+            console.log('read the deck function');
+            cardReader(file);
+        });
 
-            }]).then(function(answers) {
-                console.log('\ntime to study');
-                console.log('read the deck');
-
-
-            });
-        fs.appendFile(file, obj,function(err){
-            if (err) throw err;
-                console.error(err);
-            });
     }
 
 }
+
+
+function cardReader(deckName) {
+    fs.readFile(deckName, 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+        } else if (data !== null) {
+        	redData = JSON.parse(data);
+        	console.log(redData[1].text);
+        }
+
+    });
+
+}
+
 
 
 // create the deck
